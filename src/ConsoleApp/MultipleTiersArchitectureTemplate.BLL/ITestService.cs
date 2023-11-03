a8a0db142302a939faf9a335c8b937b8b5fbe232
Rong(Rex) Fan || 樊荣 <rong.fan2020@outlook.com>
2023-11-02 21:18:28 -0400

    Initial commit

---
+++ src/ConsoleApp/MultipleTiersArchitectureTemplate.BLL/ITestService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleTiersArchitectureTemplate.BLL
+{
+    public interface ITestService
+    {
+        public void PrintHelloWorld();
+
+        public void PrintConfigInfo();
+    }
+}
---
+++ src/ConsoleApp/MultipleTiersArchitectureTemplate.BLL/TestService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace MultipleTiersArchitectureTemplate.BLL
+{
+    public class TestService : ITestService
+    {
+        private readonly IConfiguration _configuration;
+        public TestService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void PrintConfigInfo()
+        {
+            var title = _configuration["Position:Title"];
+            var name = _configuration["Position:Name"];
+
+            Console.WriteLine($"title:{title}");
+            Console.WriteLine($"name:{name}");
+        }
+
+        public void PrintHelloWorld()
+        {
+            Console.WriteLine($"Hello world!");
+        }
+    }
+}
